a9c30117ac79ee499a81b5826def745479dea412
AakashJaisinghani <aakashj.98@gmail.com>
2025-07-09 19:03:31 -0400

    Rice_Mechanics & Env change Wip
    Added rice Pouring System
    
    Changed Environment Still wip
    
    Changed ground plane

---
+++ Cooking Sim/Assets/Scripts/Food/RicePourDetector.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RicePourDetector : MonoBehaviour
+{
+
+    public float PourThreshold = 0.75f;
+
+    public Transform PourOrigin;
+    public RiceStream StreamPrefab;
+
+    public float fillLevel = 1.0f;
+    public float drainRate = 0.1f;
+    public float currentThreshold;
+
+
+
+    private bool isPouring;
+
+    private RiceStream currentStream;
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentThreshold = Mathf.Lerp(-1f,PourThreshold,fillLevel);
+        bool pourcheck = CalculateDotProduct() < currentThreshold;
+        if (pourcheck)
+            Debug.Log("Pour Water!");
+
+        if(isPouring)
+        {
+
+            fillLevel -= drainRate*Time.deltaTime;
+        }
+        if (isPouring != pourcheck)
+        {
+            isPouring = pourcheck;
+
+            if (isPouring)
+            {
+                currentStream = Instantiate(StreamPrefab, PourOrigin.position, Quaternion.identity);
+                currentStream.followSource = PourOrigin;
+                currentStream.Begin();
+            }
+            else
+            {
+                currentStream.End();
+                currentStream = null;
+            }
+        }
+    }
+
+    private float CalculateDotProduct()
+    {
+        return Vector3.Dot(transform.up, Vector3.up);
+    }
+}
