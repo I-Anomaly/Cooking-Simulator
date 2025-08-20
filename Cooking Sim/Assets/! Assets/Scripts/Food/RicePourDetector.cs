3ec2e8c093d4d20af74ec5f95189e2708aa11f8c
vanisden <vani13591@gmail.com>
2025-08-19 22:07:05 -0400

    updated UI images, and intro splash screen as well

--- Cooking Sim/Assets/! Assets/Scripts/Food/RicePourDetector.cs
+++ Cooking Sim/Assets/! Assets/Scripts/Food/RicePourDetector.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class RicePourDetector : MonoBehaviour
 {
-
     public float PourThreshold = 0.75f;
-
     public Transform PourOrigin;
     public RiceStream StreamPrefab;
 
@@ -14,39 +12,49 @@ public class RicePourDetector : MonoBehaviour
     public float drainRate = 0.1f;
     public float currentThreshold;
 
-
-
     private bool isPouring;
-
     private RiceStream currentStream;
 
-    // Update is called once per frame
+    [Header("Audio Settings")]
+    public AudioSource pourAudioSource; // Assign your pour sound AudioSource here
+
     void Update()
     {
         currentThreshold = Mathf.Lerp(-1f, PourThreshold, fillLevel);
         bool pourcheck = CalculateDotProduct() < currentThreshold;
-        if (pourcheck)
-            // Debug.Log("Pour Water!");
 
-            if (isPouring)
-            {
+        if (isPouring)
+        {
+            fillLevel -= drainRate * Time.deltaTime;
+        }
 
-                fillLevel -= drainRate * Time.deltaTime;
-            }
         if (isPouring != pourcheck)
         {
             isPouring = pourcheck;
 
             if (isPouring)
             {
+                // Start rice stream
                 currentStream = Instantiate(StreamPrefab, PourOrigin.position, Quaternion.identity);
                 currentStream.followSource = PourOrigin;
                 currentStream.Begin();
+
+                // Start pouring sound
+                if (pourAudioSource != null && !pourAudioSource.isPlaying)
+                    pourAudioSource.Play();
             }
             else
             {
-                currentStream.End();
-                currentStream = null;
+                // End rice stream
+                if (currentStream != null)
+                {
+                    currentStream.End();
+                    currentStream = null;
+                }
+
+                // Stop pouring sound
+                if (pourAudioSource != null && pourAudioSource.isPlaying)
+                    pourAudioSource.Stop();
             }
         }
     }
@@ -58,9 +66,11 @@ public class RicePourDetector : MonoBehaviour
             currentStream.End();
             currentStream = null;
         }
-        isPouring = false;
 
-        // Disable this script to stop pouring
+        if (pourAudioSource != null && pourAudioSource.isPlaying)
+            pourAudioSource.Stop();
+
+        isPouring = false;
         this.enabled = false;
     }
 
